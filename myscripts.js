01a245206024cd81ab8b3f9b3fa7558116a7a68e
chasespigarelli <78243585+chasespigarelli@users.noreply.github.com>
2021-03-12 20:17:28 -0700

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,23 @@
+function openCity( evt, cityName )
+   {
+       // declare all variables
+       var index, tabcontent, tablinks;
 
+       // get all elements with class = "tabcontent" and hide them
+       tabcontent = document.getElementsByClassName("tabcontent");
+       for( index = 0; index < tabcontent.length; index++ )
+       {
+           tabcontent[ index ].style.display = "none";
+       }
+
+       // get all elements with class = "tablinks" and remove the class "active"
+       tablinks = document.getElementsByClassName("tablinks");
+       for( index = 0; index < tablinks.length; index++ )
+       {
+           tablinks[ index ].className = tablinks[ index ].className.replace(" active", "");
+       }
+
+       // show the current tab, and add an "active" class to the button that opened that tab
+       document.getElementById( cityName ).style.display = "block";
+       evt.currentTarget.className += " active";
+   }
