84caa4cd4b82277b6170cb0f0eecd89827e81e9a
xlilos <ia324@nau.edu>
2021-04-11 18:11:10 -0700

    fixed php get

---
+++ Website/getpaper.php
@@ -0,0 +1,27 @@
+<?php
+$filename = $_POST['fileName'];
+$dir = '/var/www/html/DoneGrading';
+$array = scandir($dir);
+$flag = -1;
+
+foreach($array as $value)
+{
+  if(strcmp($value, $filename) === 0)
+  {
+    header('Content-Type: application/force-download');
+    header("Cache-Control: private",false);
+    header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+    header('Content-Length: '.filesize($filename));
+    header("Content-Transfer-Encoding: binary");
+    readfile('DoneGrading/'.$filename);
+    $flag = 1;
+    break;
+  }
+}
+
+if($flag === -1)
+{
+  echo "File not found. Please try another file name.";
+}
+
+?>
